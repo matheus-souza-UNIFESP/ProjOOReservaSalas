7e8ef903f6df01d7b30e6779f61832c65836aa7a
Murilo <gdo.murilo@gmail.com>
2026-05-11 16:35:45 -0300

    feat: strategy pattern and fix reserva

--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -1,20 +1,31 @@
 package model;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 import observer.Assunto;
 
 public class Reserva extends Assunto {
+    private LocalDateTime diaEHoraQueOcorreuAReserva;
     private Date dia;
     private StatusReserva status;
     private Sala sala;
     private Pessoa pessoa;
 
-    public Reserva(Date dia, StatusReserva status, Sala sala, Pessoa pessoa) {
+    public Reserva(LocalDateTime diaEHoraQueOcorreuAReserva, Date dia, StatusReserva status, Sala sala, Pessoa pessoa) {
+        this.diaEHoraQueOcorreuAReserva = diaEHoraQueOcorreuAReserva;
         this.dia = dia;
         this.status = status;
         this.sala = sala;
         this.pessoa = pessoa;
+    }
+
+    public void setDiaQueOcorreuAReserva(LocalDateTime dia) {
+        this.diaEHoraQueOcorreuAReserva = dia;
+    }
+
+    public LocalDateTime getDiaQueOcorreuAReserva() {
+        return diaEHoraQueOcorreuAReserva;
     }
 
     public void setDia(Date dia) {
