f500047b1eacfa7ea3dcf7854e5053ea468a6491
matheus-souza-UNIFESP <kawasaki-campos@unifesp.com>
2026-05-12 12:19:06 -0300

    feat: Adicionado SistemaDeReserva

--- src/service/SistemaDeReservas.java
+++ src/service/SistemaDeReservas.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import model.Sala;
 import model.Pessoa;
@@ -32,6 +33,27 @@ public class SistemaDeReservas {
         return instancia;
     }
 
+    private Reserva achaReserva(Sala sala, Date data) {
+        for(Reserva reserva : reservas) {
+            if(reserva.getSala().equals(sala) && reserva.getDia().equals(data)) {
+                return reserva;
+            }
+        }
+
+        return null;
+    }
+
+    private List<Reserva> sortReservas(List<Reserva> aOrdenar) {
+        aOrdenar.sort(
+            Comparator
+                .comparing(Reserva::getDia)
+                .thenComparing(
+                    reserva -> reserva.getSala().getNumero()
+                )
+        );
+
+        return aOrdenar;
+    }
 
     public List<Sala> listarSalasDisponiveis(Date dataInicio, Date dataFinal) {
         List<Sala> disponiveis = new ArrayList<>();
@@ -57,53 +79,73 @@ public class SistemaDeReservas {
         return disponiveis;
     }
 
-    private Reserva achaReserva(Sala sala, Date data) {
+    public Resultado reservarSala(Sala sala, Pessoa pessoa, Date data) {
+        Reserva novaReserva = new Reserva(LocalDateTime.now(), data, StatusReserva.PENDENTE, sala, pessoa);
+        Reserva jaExiste = achaReserva(sala, data);
+        PoliticaDeReserva politica = new PrimeiroAReservar(); 
+       
+        if(jaExiste != null) {
+            if(politica.verificaColisao(jaExiste, novaReserva) == jaExiste) {
+                politica = new PrioridadeParaDocente();
+                if(politica.verificaColisao(jaExiste, novaReserva) == jaExiste) {
+                    return Resultado.JA_EXISTE;
+                }
+            }
+
+            jaExiste.notificarTodos(jaExiste);
+            reservas.remove(jaExiste);
+        }
+
+        reservas.add(novaReserva);
+        return Resultado.SUCESSO;
+    }
+
+    public Resultado cancelarReserva(Sala sala, Pessoa pessoa, Date data) {
+        Reserva reserva = achaReserva(sala, data);
+
+        if(reserva == null){
+            return Resultado.NAO_ENCONTRADO;
+        }
+
+        if(!reserva.getPessoa().equals(pessoa)) {
+            return Resultado.NAO_AUTORIZADO;
+        }
+        
+        reservas.remove(reserva);
+        return Resultado.SUCESSO;
+    }
+
+    public Resultado editarReserva(Sala sala, Pessoa pessoa, Date data, Sala novaSala, Date novaData) {
+        Reserva aEditar = achaReserva(sala, data);
+
+        if(aEditar == null){
+            return Resultado.NAO_ENCONTRADO;
+        }
+
+        if(!aEditar.getPessoa().equals(pessoa)) {
+            return Resultado.NAO_AUTORIZADO;
+        }
+
+
+        reservas.remove(aEditar);
+        
+        Resultado res = reservarSala(novaSala, pessoa, novaData);
+        if(res.equals(Resultado.JA_EXISTE)) {
+            reservas.add(aEditar);
+        }
+
+        return res;
+    }
+
+    public List<Reserva> relatorioDiario(Date data) {
+        List<Reserva> relatorio = new ArrayList<>();
+        
         for(Reserva reserva : reservas) {
-            if(reserva.getSala().equals(sala) && reserva.getDia().equals(data)) {
-                return reserva;
+            if(reserva.getDia().equals(data)) {
+                relatorio.add(reserva);
             }
         }
 
-        return null;
-    }
-
-    public Reserva reservarSala(Sala sala, Pessoa pessoa, Date data) {
-        Reserva novaReserva = new Reserva(LocalDateTime.now(), data, StatusReserva.PENDENTE, sala, pessoa);
-        Reserva jaExiste = achaReserva(sala, data);
-
-        PoliticaDeReserva politica = new PrimeiroAReservar(); 
-       
-        if(jaExiste == null) {
-            reservas.add(novaReserva);
-            return novaReserva;
-        }
-        
-        if(politica.verificaColisao(jaExiste, novaReserva) == novaReserva) {
-            jaExiste.notificarTodos(jaExiste);
-            reservas.remove(jaExiste);
-            
-            reservas.add(novaReserva);
-            return novaReserva;
-        }
-
-        politica = new PrioridadeParaDocente();
-        if(politica.verificaColisao(jaExiste, novaReserva) == novaReserva) {
-            jaExiste.notificarTodos(jaExiste);
-            reservas.remove(jaExiste);
-
-            reservas.add(novaReserva);
-            return novaReserva;
-        }
-        
-       return null;
-    }
-
-    public void cancelarReserva(Reserva reserva) {
-        if(achaReserva(reserva.getSala(), reserva.getDia()) == null){
-            System.out.println("Reserva não encontrada");
-        } else {
-            reservas.remove(reserva);
-            System.out.println("Reserva cancelada com sucesso");
-        }
+        return sortReservas(relatorio);
     }
 }
